eb673bfc2c901ac83f5787af77ea99a043bb1dfa
hodaka <altitude3190@gmail.com>
2014-12-23 22:03:02 +0900

    modify episode commands tests

--- spec/clis/episode_spec.rb
+++ spec/clis/episode_spec.rb
@@ -2,32 +2,30 @@ require 'spec_helper'
 
 describe Rubipara::Episode do
 
-  describe '.config' do
-    context 'when config is loaded' do
-      config = Rubipara::Episode.load_config
-      it 'should have some text of YAML format' do
-        expect(config.length).to be > 0
+  describe '.new' do
+    context 'when the arg is an episode number which has been already broadcast' do
+      episode = Rubipara::Episode.new 2
+      it 'should initialize with its arg' do
+        expect(episode.episode_num).to be === 2
+      end
+    end
+    context 'with invalid args' do
+      it 'should throw an exception of RuntimeError' do
+        expect{Rubipara::Episode.new 'invalid'}.to raise_error(RuntimeError, 'ERROR: No such an episode')
+      end
+    end
+    context 'with no args' do
+      it 'should throw an exception of ArgumentError' do
+        expect{Rubipara::Episode.new}.to raise_error(ArgumentError)
       end
     end
   end
 
-  describe '.new' do
-    context 'with no args' do
-      episode = Rubipara::Episode.new
-      it 'should initialize with its arg being 1' do
-        expect(episode.episode['title']).to eq('アイドル始めちゃいました！')
-      end
-    end
-    context 'with args' do
-      episode = Rubipara::Episode.new(2)
-      it 'should initialize with its arg' do
-        expect(episode.episode['title']).to eq('約束やぶっちゃダメぷりっ')
-      end
-    end
-    context 'with invalid args' do
-      episode = Rubipara::Episode.new('invalid')
-      it 'should substitute 0 for episode_num' do
-        expect(episode.episode_num).to be === 0
+  describe '.load_config' do
+    context 'when config is loaded' do
+      config = Rubipara::Episode.load_config
+      it 'should have some text of YAML format' do
+        expect(config.length).to be > 0
       end
     end
   end
